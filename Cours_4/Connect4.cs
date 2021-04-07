266901d7104e0166e64ad9a46f47285f2b2d7d4e
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-07 18:13:59 +0200

    premier commit

---
+++ Cours_4/Connect4.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cours_4
+{
+    class Connect4
+    {
+        int[,] table = new int[13, 14];
+        int player = 0;
+        private int[] playerPos = new int[2] {0, 0};
+
+
+        public int LineCount
+        {
+            get { return 12; }
+        }
+
+        public int ColCount => 13;
+
+        public char GetPawn(int col, int line)
+        {
+            char result = ' ';
+
+            if (table[col, line] == 1)
+            {
+                return result = 'O';
+            }
+            else if (table[col, line] == 2)
+            {
+                return result = 'X';
+            }
+            else
+            {
+                return result = ' ';
+            }
+        }
+
+        public int PlayerNumber()
+        {
+            if (player == 1)
+            {
+                player++;
+            }
+            else if (player == 2)
+            {
+                player--;
+            }
+            else
+            {
+                player++;
+            }
+
+            return player;
+        }
+
+        public void Play(int column)
+        {
+            column += 2;
+            int i = LineCount - 4;
+
+            for (;;)
+            {
+                if (i == 2)
+                {
+                    throw new ArgumentException("Choose an other column. This one is full.");
+                    Console.WriteLine("Choose an other column. This one is full.");
+                    player--;
+                    break;
+                }
+                else if (table[column, i] == 0)
+                {
+                    table[column, i] = player;
+
+                    playerPos[0] = column;
+                    playerPos[1] = i;
+                    break;
+                }
+                else if (table[column, i] == 1 || table[column, i] == 2)
+                {
+                    i--;
+                }
+            }
+        }
+
+        public int Winner()
+        {
+            int x = playerPos[0], y = playerPos[1];
+            for (int i = -3; i < 0; i++)
+            {
+                if (table[x + i, y] == player && 
+                    table[x + i + 1, y] == player && 
+                    table[x + i + 2, y] == player &&
+                    table[x + i + 3, y] == player)
+                {
+                    return player;
+                }
+                else if (table[x, y] == player && 
+                         table[x, y - 1] == player && 
+                         table[x, y - 2] == player &&
+                         table[x, y - 3] == player)
+                {
+                    return player;
+                }
+                else if (table[x + i, y - i] == player && 
+                         table[x + i + 1, y - i - 1] == player &&
+                         table[x + i + 2, y - i - 2] == player &&
+                         table[x + i + 3, y - i - 3] == player)
+                {
+                    return player;
+                }
+                else if (table[x - i, y - i] == player && 
+                         table[x - i - 1, y - i - 1] == player &&
+                         table[x - i - 2, y - i - 2] == player &&
+                         table[x - i - 3, y - i - 3] == player)
+                {
+                    return player;
+                }
+            }
+            
+
+            if (table[3, 3] != 0 && table[4, 3] != 0 && table[5, 3] != 0 && table[6, 3] != 0 && table[7, 3] != 0 &&
+                table[8, 3] != 0 && table[9, 3] != 0)
+            {
+                return 3;
+            }
+
+            return 0;
+        }
+
+
+        public bool Ended()
+        {
+            bool cond = Winner() != 0;
+            return cond;
+        }
+    }
+}
---
+++ Cours_4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Cours_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Connect4();
+            Regex rx = new Regex(@"^[+]?[1-7]+$");
+            do
+            {
+                Display(game);
+                for (;;)
+                {
+                    Console.WriteLine($"Player {game.PlayerNumber()} : Which column 1-{game.ColCount-6} ?");
+
+                    var turn = Console.ReadLine();
+                    int column;
+                    if (!rx.IsMatch(turn))
+                    {
+                        Console.Error.WriteLine("Invalid input.");
+                    }
+                    else if (Int32.Parse(turn) < 1 || Int32.Parse(turn) > game.ColCount)
+                    {
+                        Console.Error.WriteLine("Invalid column number.");
+                    }
+                    else if (int.TryParse(turn, out column))
+                    {
+                        game.Play(column);
+                        break;
+                    }
+                }
+            } while (!game.Ended());
+
+            Display(game);
+            if (game.Winner() == 3)
+            {
+                Console.WriteLine("Draw");
+            }
+            else
+            {
+                Console.WriteLine($"Player {game.Winner()} wins.");
+            }
+        }
+
+     
+
+        private static void Display(Connect4 game)
+        {
+            for (int y = 3; y < game.LineCount - 3; y++)
+            {
+                for (int x = 3; x < game.ColCount - 3; x++)
+                {
+                    Console.Write($"| {game.GetPawn(x, y)} ");
+                }
+
+                Console.WriteLine("|");
+                for (int x = 3; x < game.ColCount - 3; x++)
+                {
+                    Console.Write($"+---");
+                }
+
+                Console.WriteLine("+");
+            }
+
+            for (int x = 3; x < game.ColCount - 3; x++)
+            {
+                Console.Write($"  {x - 2} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
